769b49ff127d434bee5b0f07fa59fec6558724e9
James <jimibue@gmail.com>
2022-04-20 11:04:26 -0700

    brain tree

--- app/controllers/api/braintree_controller.rb
+++ app/controllers/api/braintree_controller.rb
@@ -1,7 +1,33 @@
 class Api::BraintreeController < ApplicationController
+  # gives the token we need to setup the UI
   def token
+    render json: ENV['BRAINTREE_DROPIN_TOKEN']
   end
 
+  # => amount, nonce...
+  # => transaction id if successfull.
   def payment
+
+    result = Braintree::Transaction.sale(
+      :amount => params[:amount],
+      :payment_method_nonce => params[:nonce],
+      :options => {
+        # charge the card right now
+        :submit_for_settlement => true
+      }
+    )
+
+    # success
+    if result.success?
+      render json: result.transaction.id
+    # something with the card  
+    elsif result.transaction
+      text = "text: #{result.transaction.processor_response_text}"
+      code = "code: #{result.transaction.processor_response_code}"
+      render json: { errors: { text: text, code: code } }
+     # something went wrong not on braintrees end (me problem)  
+    else
+      render json: { errors: result.errors, }, status: 422
+    end
   end
 end
--- client/src/components/shared/BraintreeDemo.js
+++ client/src/components/shared/BraintreeDemo.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, {  useEffect, useState } from "react";
 import braintree from "braintree-web-drop-in";
 import BraintreeDropin from "braintree-dropin-react";
 import BraintreeSubmitButton from "./BrainTreeSubmitButton";
+import axios from "axios";
 
 const BraintreeDemo = (props) => {
-  const handlePaymentMethod = (payload) => {
+  // state = { loaded: false, token: '', };
+  const [loaded, setLoaded] = useState(false);
+  const [token, setToken] = useState('');
+  const [amount, setAmount] = useState(100);
+
+  useEffect(()=>{
+    getToken()
+  },[])
+
+  const getToken = async()=>{
+    let res = await axios.get('/api/braintree_token')
+    setToken(res.data)
+    setLoaded(true)
+  }
+  const handlePaymentMethod = async (payload) => {
       console.log('payload', payload)
     //TODO: Make axios call to our server to post a payment
+    // amount, nonce
+    try{
+       let res = await axios.post('/api/payment', {amount, ...payload})
+       // expecting success
+       console.log('transaction id?', res.data)
+       //  store braintree_transaction.id to your db, 
+       // redirect to like transaction page and show user the transaction id
+    }catch(err){ 
+      // have some good error handling here for ui/ux
+      console.log('err', err)
+      console.log('err', err.response)
+    }
   };
+  if(!loaded){
+    return <p>spinner</p>
+  }
   return (
     <div>
       <h1>BT demo</h1>
+      <p>amount</p>
+      <input value={amount} onChange={(e)=> setAmount(e.target.value)} />
       <BraintreeDropin
         braintree={braintree}
-        authorizationToken={"sandbox_fw5b3ftz_zmsjhgxyrsw6cfvh"}
+        authorizationToken={token}
         handlePaymentMethod={handlePaymentMethod}
         renderSubmitButton={BraintreeSubmitButton}
       />
